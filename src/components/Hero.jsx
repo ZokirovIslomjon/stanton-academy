892f9958cb062f1d5d42dfc0c52f68ebc5ce3a88
ZokirovIslomjon <izokirov905@gmail.com>
2026-05-27 20:43:57 +0800

    Fixed size summer camp poster

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 // Make sure this path is correct for your project!
-import summerCampImg from '../assets/landing.jpg'; 
+import summerCampImg from '../assets/landing.png'; 
 
 const Hero = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
